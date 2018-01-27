0359e13b244bb6934016a7416b7b1a1385333d7a
Peter bock (@pbock on steemit) <bockpeter@hotmail.com>
2018-01-27 15:22:32 +0100

    Update and rename test_functions.py to test_Hello.py

---
+++ test_Hello.py
@@ -0,0 +1,9 @@
+from functScript import *
+
+def test_hello():
+    assert hello("travis") == "hello travis"
+
+    
+    
+    
+
--- test_functions.py
+++
@@ -1,8 +0,0 @@
-from functScript import *
-from nonfunctScript import *
-
-def test_hello():
-    assert hello("travis") == "hello travis"
-
-def test_goodbye():
-    assert goodbye("travis") == "Goodbye travis"
