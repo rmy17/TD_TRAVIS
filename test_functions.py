7c62a957db0b51be22f9266617738e0a17d0010a
Peter bock (@pbock on steemit) <bockpeter@hotmail.com>
2018-01-27 15:19:41 +0100

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -2,7 +2,7 @@ from functScript import *
 from nonfunctScript import *
 
 def test_hello():
-    assert hello("travis") == "Hello travis"
+    assert hello("travis") == "hello travis"
 
 def test_goodbye():
     assert goodbye("travis") == "Goodbye travis"
